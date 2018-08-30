76d9474e7fa720daf8a3b304d818429bba94d7f2
Inho Jung <inhojung@outlook.com>
2018-08-30 14:05:22 +0900

    clean

--- weeks/src/main/java/com/weeks/hello/service/HelloService.java
+++
@@ -1,9 +0,0 @@
-package com.weeks.hello.service;
-
-import java.util.List;
-
-import com.weeks.hello.vo.Hello;
-
-public interface HelloService {
-	public List<Hello> findAll(); 
-}
--- weeks/src/main/java/com/weeks/user/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.weeks.user.service;
-
-import java.util.List;
-
-import com.weeks.user.vo.User;
-
-public interface UserService {
-	public List<User> findAll();
-}
