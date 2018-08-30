d1158413a08e19ebe9a8fe45556a29ea82eda8b9
Inho Jung <inhojung@outlook.com>
2018-08-30 14:06:49 +0900

    clean

--- weeks/src/main/java/com/weeks/hello/dao/HelloDAO.java
+++
@@ -1,9 +0,0 @@
-package com.weeks.hello.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.weeks.hello.vo.Hello;
-
-public interface HelloDAO extends JpaRepository<Hello, Integer>{
-	
-}
--- weeks/src/main/java/com/weeks/user/dao/UserDAO.java
+++
@@ -1,9 +0,0 @@
-package com.weeks.user.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.weeks.user.vo.User;
-
-public interface UserDAO extends JpaRepository<User, Integer>{
-
-}
